bfa465949640f8d300a5cf76774145da15954154
Ana Fernandez <anafzcz@gmail.com>
2025-02-02 23:35:30 -0400

    first commit

---
+++ .history/app_20250202232508.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = ("Juego de número secreto");
+
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Escoja un número entre 1 y 10';
---
+++ app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = ("Juego de número secreto");
+
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Escoja un número entre 1 y 10';
+
+function verificarChute(){
+   console.log('El botón fue cliqueado');
+}
