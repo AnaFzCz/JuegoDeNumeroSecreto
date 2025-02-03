1375e2611762d400c901f093685cc7d0918ea0a6
Ana Fernandez <anafzcz@gmail.com>
2025-02-03 00:26:06 -0400

    Funciones con parametros

---
+++ .history/app_20250203001706.js
@@ -0,0 +1,20 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = ("Juego de número secreto");
+
+
+// let parrafo = document.querySelector('p');
+// parrafo.innerHTML = 'Escoja un número entre 1 y 10';
+
+function exibirTextoNaTela(tag, texto){
+let campo = document.querySelector(tag);
+campo.innerHTML=texto;
+}
+
+exibirTextoNaTela('h1','Juego de Número Secreto');
+exibirTextoNaTela('p','Elige un número entre 1 y 10');
+
+
+
+function verificarChute(){
+   console.log('El botón fue cliqueado');
+}
